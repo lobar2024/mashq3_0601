775f63b23b6b5cd49c3887349c4d1d5c4c3f0eff
Lobar Farxodova <qodirovjavohir2109@gmail.com>
2026-01-06 19:25:59 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+# 3. JUFT YOKI TOQ
+number = int(input("Son kiriting: "))  # son kiritish
+
+if number % 2 == 0:  # 2 ga bo‘linishini tekshirish
+    print("Juft")    # juft bo‘lsa
+else:
+    print("Toq")     # toq bo‘lsa
